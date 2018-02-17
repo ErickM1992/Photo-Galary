a710f87cd1a9425c0a89fc462d701e7d27be22dc
ErickM1992 <moreiraerick1@hotmail.com>
2018-02-16 18:26:10 -0600

    Strat commit
    This is the start proyect wicht 0% of changes

---
+++ MyPhotoGalery/MyPhotoGalery/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyPhotoGalery.MvcApplication" Language="C#" %>
---
+++ MyPhotoGalery/MyPhotoGalery/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyPhotoGalery.Startup))]
+namespace MyPhotoGalery
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
